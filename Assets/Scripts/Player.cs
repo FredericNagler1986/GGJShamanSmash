c690db9c0c55856b31ed3d71a4ca7f34e4534ff3
Kutsarit Vanthavongsee <Kutsarit@web.de>
2016-01-30 02:29:21 +0100

    add player testing

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public string inputPrefix;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		var x = Input.GetAxis ( inputPrefix + "Horizontal" );
+		var y = Input.GetAxis ( inputPrefix + "Vertical" );
+
+		var btnX = Input.GetButton ( inputPrefix + "X" );
+		var btnY = Input.GetButton ( inputPrefix + "Y" );
+		var btnA = Input.GetButton ( inputPrefix + "A" );
+		var btnB = Input.GetButton ( inputPrefix + "B" );
+
+		transform.position += new Vector3 ( x, y, 0 );
+
+		Debug.Log ( "X: " + btnX + "; Y: " + btnY + "; A: " + btnA + "; B: " + btnB );
+	}
+}
