f5e510eac91c588d9574df659ab80dbd62589c97
Kutsarit Vanthavongsee <Kutsarit@web.de>
2016-01-30 19:22:09 +0100

    add player action data add action effect

---
+++ Assets/Scripts/PlayerActionManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PlayerActionManager : MonoBehaviour
+{
+	public Content Content;
+
+	public bool PlayAction(Player owner, List<OrbType> orbList)
+	{
+		if ( orbList.Count == 0 )
+		{
+			return false;
+		}
+		
+		foreach ( PlayerAction action in Content.Actions )
+		{
+			bool isOk = true;//action.OrbNeeded.SequenceEqual ( orbList );
+			if ( isOk )
+			{
+				return ExecuteAction ( owner, action );
+			}
+		}
+		return false;
+	}
+
+	private bool ExecuteAction( Player owner, PlayerAction action )
+	{
+		if ( action.EffectPrefab == null )
+		{
+			return false;
+		}
+
+		var instance = Object.Instantiate ( action.EffectPrefab );
+
+		instance.BroadcastMessage ( "OnExecuteAction", new object[] { owner, action } );
+
+		return true;
+	}
+}
