f92b08302dff9bab428ccf1175481aec4b1205b2
Frederic Nagler <frederic.nagler1986@gmail.com>
2016-01-30 17:46:13 +0100

    gamemanager first steps

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    public void Init()
+    {
+
+    }
+
+    private void AddPlayer(int playerId)
+    {
+
+    }
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
