bd43cab320b021f91e8fd6ca08f6da5efd0d48da
Lirion <ltruemper@gmx.de>
2016-01-30 15:20:48 +0100

    Updated Main
    added GameState

---
+++ Assets/Scripts/EGameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public enum EGameState
+{
+    None,
+    MainMenu,
+    StageSelection,
+    CharacterSelection,
+    Ingame,
+    Result,
+    Credits,
+    Options
+}
