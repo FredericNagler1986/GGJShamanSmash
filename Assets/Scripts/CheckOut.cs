cfb9140072763365b489864384211345853b6161
Frederic Nagler <frederic.nagler1986@gmail.com>
2016-01-30 02:03:58 +0100

    testlevel added

---
+++ Assets/Scripts/CheckOut.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckOut : MonoBehaviour {
+
+    public Vector2 Area;
+
+    private float radius;
+
+    private Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+        radius = GetComponent<CircleCollider2D>().radius;
+        rigid = GetComponent<Rigidbody2D>();
+	}
+
+    void Update()
+    {
+        var velocity = rigid.velocity;
+
+        if(velocity.x > 0 && (transform.position.x + radius) > Area.x )
+        {
+            var temp = transform.position;
+            temp.x = radius - Area.x;
+            transform.position = temp;
+        }
+        else if (velocity.x < 0 && (transform.position.x - radius) < -Area.x)
+        {
+            var temp = transform.position;
+            temp.x = Area.x - radius;
+            transform.position = temp;
+        }
+        else if ((transform.position.y - radius) < - Area.y)
+        {
+            var temp = transform.position;
+            temp.y = Area.y - radius;
+            transform.position = temp;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        var p1 = new Vector3(Area.x, Area.y, 0);
+        var p2 = new Vector3(-Area.x, Area.y, 0);
+        var p3 = new Vector3(-Area.x, -Area.y, 0);
+        var p4 = new Vector3(Area.x, -Area.y, 0);
+
+        Gizmos.DrawLine(p1, p2);
+        Gizmos.DrawLine(p2, p3);
+        Gizmos.DrawLine(p3, p4);
+        Gizmos.DrawLine(p4, p1);    }
+}
