6807085857a99b3e9f0d0d4ddba693d71669da6a
Lirion <ltruemper@gmx.de>
2016-01-30 17:44:44 +0100

    Added Character selection

---
+++ Assets/Scripts/UI/PlayerSelection.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PlayerSelection : SingletonMonoBehaviour<PlayerSelection>
+{
+    public Button ToLevelSelectionButton = null;
+    private List<PlayerSelectionObject> mPlayerSelectionObjects = new List<PlayerSelectionObject>();
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        Main.Instance.GameState = EGameState.CharacterSelection;
+        for (int i = 0; i < mPlayerSelectionObjects.Count; i++)
+        {
+            mPlayerSelectionObjects[i].Status = EPlayerSelectionStatus.Inactive;
+        }
+    }
+
+    public void UpdateButton()
+    {
+        bool active = true;
+        int counter = 0;
+        for (int i = 0; i < mPlayerSelectionObjects.Count; i++)
+        {
+            if (mPlayerSelectionObjects[i].Status == EPlayerSelectionStatus.SelectingMask )
+            {
+                active = false;
+            }
+            if (mPlayerSelectionObjects[i].Status == EPlayerSelectionStatus.Ready)
+            {
+                counter++;
+            }
+        }
+        active = counter > 1;
+        if (ToLevelSelectionButton != null)
+        {
+            ToLevelSelectionButton.interactable = active;
+        }
+    }
+
+    public void RegisterPlayerControl(PlayerSelectionObject plObj)
+    {
+        if (!mPlayerSelectionObjects.Contains(plObj))
+        {
+            mPlayerSelectionObjects.Add(plObj);
+        }
+    }
+}
