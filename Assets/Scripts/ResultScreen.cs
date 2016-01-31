cf6b5d8a4b22477d46a7041c5f8f3dc5db9e1a52
Frederic Nagler <frederic.nagler1986@gmail.com>
2016-01-31 09:22:13 +0100

    resultscreen updated

---
+++ Assets/Scripts/ResultScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void BackToMainMenue()
+    {
+        Time.timeScale = 1.0f;
+        LevelManager.Instance.LoadAndFadeToLevel("MainMenu");
+    }
+}
